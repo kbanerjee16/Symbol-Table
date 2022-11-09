4b255cf878291de16ee9398aa7b9fbed409dbd51
Banerjee <KBANERJEE@friendsbalt.org>
2022-11-09 15:23:06 -0500

    end of class

--- SymbolTable.java
+++ SymbolTable.java
@@ -1,32 +1,45 @@
 public class SymbolTable <Key extends Comparable<Key>, Value> {
-    Node root;
-    class Node {
+    Node root;//first node, first thing in the tree
+    private class Node {
         Key k;
         Value v;
         Node left;
         Node right;
 
-        public Node(Key k, Value v, Node left, Node right) {
+        public Node(Key k, Value v) {
             this.k = k;
             this.v = v;
-            this.left = left;
-            this.right = right;
+			this.left = null;
+			this.right = null;
         }
+		
+		private Node put(Node x, Key k, Value v) {
+			if (x == null) {
+				return new Node(k, v);
+			}
+			
+			int cmp = k.compareTo(x.k);
+			if (cmp > 0) {
+				x.right = put(x.right, k, v);
+			}
+			if (cmp < 0) {
+				x.left = put(x.left, k, v);
+			}
+			return x;
+		}
 
-        public Key getKey() {
-            return this.k;
+        private Node select(Node x, int k) {
+            if (x == null)
+				return null;
         }
     }
-    public SymbolTable() {
-        
-    }
+	
+	public Key select(int k) {
+			return select(root, k);
+		}
 
     public void put(Key k, Value v) {
-        if (k < root.k) {
-            Node temp = root;
-            root = Node(k, v, temp, null);
-        }
-        //put it in the right place
+		root = put(root, k, v);
     }
 
     public Value get(Key k) {
